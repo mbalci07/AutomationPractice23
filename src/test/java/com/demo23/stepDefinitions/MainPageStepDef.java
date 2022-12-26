4daf45e65c78b3d25ae8a5d7a9d19473b7e1f3de
Berk620 <kifmyvz@gmail.com>
2022-12-26 22:18:24 +0100

    Improvement

--- src/test/java/com/demo23/stepDefinitions/MainPageStepDef.java
+++ src/test/java/com/demo23/stepDefinitions/MainPageStepDef.java
@@ -1,15 +1,18 @@
 package com.demo23.stepDefinitions;
 
+import com.demo23.pages.BasePage;
 import com.demo23.pages.DashBoardPage;
+import com.demo23.utilities.BrowserUtils;
 import com.demo23.utilities.ConfigurationReader;
 import com.demo23.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
 
-public class MainPageStepDef {
+public class MainPageStepDef extends BasePage {
+
     DashBoardPage dashBoardPage = new DashBoardPage();
+
     @Given("User opens the given link")
     public void userOpensTheGivenLink() {
         Driver.get().get(ConfigurationReader.get("url"));
@@ -18,5 +21,26 @@ public class MainPageStepDef {
     @Then("User verify the Dashboard is opened")
     public void userVerifyTheDashboardIsOpened() {
         Assert.assertTrue(dashBoardPage.VerifyDashboard());
+    }
+
+    @Given("User is landed to demoqa.com page")
+    public void user_is_landed_to_demoqa_com_page() {
+        System.out.println("Opening the home page");
+        //driver ==Driver.get()  //Driver.get()--> this gets the webdriver
+        String url = ConfigurationReader.get("url");
+        Driver.get().get(url);
+    }
+
+    @Then("The page title is {string}")
+    public void the_page_title_is(String title) {
+        BrowserUtils.waitFor(2);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals(title, actualTitle);
+    }
+
+    @Then("Navigate to {string} page")
+    public void navigateToPage(String pageName) {
+        dashBoardPage.seleniumOnlineTraining.click();
+        dashBoardPage.waitForVisibilityOfElement(dashBoardPage.tutorials);
     }
 }
